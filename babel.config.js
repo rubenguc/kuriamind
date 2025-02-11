568912d6ac0b544dee080f5ccd3b5b7f00d8bfa5
Ruben <rubdeivis@gmail.com>
2025-02-10 20:55:58 -0400

    chore: Config for absolute imports

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+  ],
 };
