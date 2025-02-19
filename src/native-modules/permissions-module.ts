836ead882a1bcda4650c21baa98fd5bea1a5bae8
Ruben <rubdeivis@gmail.com>
2025-02-19 19:37:18 -0400

    feat: Accesibility permissions

--- src/native-modules/permissions-module.ts
+++ src/native-modules/permissions-module.ts
@@ -2,15 +2,20 @@ import {NativeModules} from 'react-native';
 
 const {PermissionsModule} = NativeModules;
 
-export const openNotificationSettings = () => {
-  PermissionsModule.openNotificationSettings();
+// NotificationListenerService
+export const openNotificationListenerSettings = (): void => {
+  PermissionsModule.openNotificationListenerSettings();
 };
 
-export const isNotificationServiceEnabled = async () => {
-  try {
-    const isEnabled = await PermissionsModule.isNotificationServiceEnabled();
-    console.log('Servicio de accesibilidad habilitado:', isEnabled);
-  } catch (error) {
-    console.error('Error verificando el servicio:', error);
-  }
+export const checkNotificationListenerServiceEnabled = (): Promise<boolean> => {
+  return PermissionsModule.checkNotificationListenerServiceEnabled();
+};
+
+// AccessibilityService
+export const openAccessibilitySettings = (): void => {
+  PermissionsModule.openAccessibilitySettings();
+};
+
+export const checkAccessibilityServiceEnabled = (): Promise<boolean> => {
+  return PermissionsModule.checkAccessibilityServiceEnabled();
 };
