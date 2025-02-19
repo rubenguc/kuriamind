92a7a676bd2359f3b2bbaea32bf3321f09b4a3a1
Ruben <rubdeivis@gmail.com>
2025-02-19 15:17:46 -0400

    feat: Permissions module

---
+++ android/app/src/main/java/com/kuriamaindo/modules/permissions/PermissionsPackage.kt
@@ -0,0 +1,18 @@
+package com.kuriamaindo.modules.permissions
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+class PermissionsPackage : ReactPackage {
+    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+        return listOf(PermissionsModule(reactContext))
+    }
+
+    override fun createViewManagers(
+            reactContext: ReactApplicationContext
+    ): List<ViewManager<*, *>> {
+        return emptyList()
+    }
+}
