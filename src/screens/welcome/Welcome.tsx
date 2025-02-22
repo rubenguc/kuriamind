a6a9d2e6a91f2613cf8fc257bc7e35b929dff9f9
Ruben <rubdeivis@gmail.com>
2025-02-22 10:23:38 -0400

    refactor: i18n for welcome screen

--- src/screens/welcome/Welcome.tsx
+++ src/screens/welcome/Welcome.tsx
@@ -6,6 +6,7 @@ import {Text} from '@/components/ui/text';
 import {useTranslation} from 'react-i18next';
 import {Heading} from '@/components/ui/heading';
 import {Permissions} from './components';
+import {VStack} from '@/components/ui/vstack';
 
 const Welcome = ({navigation}) => {
   const {t} = useTranslation('welcome');
@@ -24,25 +25,28 @@ const Welcome = ({navigation}) => {
     <Box className="flex h-full">
       <ScrollView className="flex-1 px-10 py-5">
         {step === Step.WELCOME ? (
-          <>
-            <Heading className="text-center mb-3 text-3xl">
+          <VStack className="mt-10 gap-10">
+            <Heading className="text-center mb-3 text-3xl font-bold">
               {t('title')}
             </Heading>
-            <Text>{t('description')}</Text>
-          </>
+            <Text className="text-white text-lg">{t('description_1')}</Text>
+            <Text className="text-white text-lg">{t('description_2')}</Text>
+          </VStack>
         ) : (
           <Permissions
             onAllPermissionsGranted={() => setAllPermissionsGranted(true)}
           />
         )}
       </ScrollView>
-      <Box className="p-4 border-t-black/10 border-t shadow-lg">
+      <Box className="p-3 border-t-black/10 border-t shadow-lg">
         <Button
           className="rounded-2xl"
           size="xl"
           isDisabled={!canGoNext()}
           onPress={handleNext}>
-          <ButtonText>Next</ButtonText>
+          <ButtonText>
+            {t(step === Step.PERMISSIONS ? 'next' : 'start_apps')}
+          </ButtonText>
         </Button>
       </Box>
     </Box>
--- src/screens/welcome/components/RequestPermissionOption.tsx
+++ src/screens/welcome/components/RequestPermissionOption.tsx
@@ -1,8 +1,8 @@
+import {TouchableOpacity} from 'react-native';
 import {HStack} from '@/components/ui/hstack';
 import {Text} from '@/components/ui/text';
 import {VStack} from '@/components/ui/vstack';
-import {ChevronRight} from 'lucide-react-native';
-import {TouchableOpacity} from 'react-native';
+import {CircleCheckBig} from 'lucide-react-native';
 
 interface RequestPermissionOptionProps {
   title: string;
@@ -22,15 +22,15 @@ export const RequestPermissionOption = ({
   return (
     <TouchableOpacity disabled={isActive} onPress={onRequestPermission}>
       <HStack
-        className={`items-center px-3 rounded-xl py-4 ${
-          isActive ? 'bg-green-500' : 'bg-gray-400'
+        className={`items-center px-3 rounded-xl py-4 border-2  bg-gray-950 ${
+          isActive ? 'border-custom-green' : 'border-gray-500'
         }`}>
         {Icon}
         <VStack className="flex-1 px-4">
-          <Text>{title}</Text>
+          <Text className="text-white font-bold mb-2">{title}</Text>
           <Text>{description}</Text>
         </VStack>
-        <ChevronRight size={16} />
+        <CircleCheckBig size={22} color={isActive ? '#9bec8f' : '#888'} />
       </HStack>
     </TouchableOpacity>
   );
