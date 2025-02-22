8ccd9009730fadcd06672d5ad9b7232b49dcdbed
Ruben <rubdeivis@gmail.com>
2025-02-22 10:58:26 -0400

    fix: Fix colors + i18n for blocks screen

--- src/screens/blocks/components/BlockSaved.tsx
+++ src/screens/blocks/components/BlockSaved.tsx
@@ -1,10 +1,14 @@
 import {SelectedAppsInBlock} from '@/components/shared';
-import {Button, ButtonIcon, ButtonText} from '@/components/ui/button';
+import {Badge, BadgeIcon, BadgeText} from '@/components/ui/badge';
+import {Box} from '@/components/ui/box';
+import {HStack} from '@/components/ui/hstack';
 import {Menu, MenuItem, MenuItemLabel} from '@/components/ui/menu';
 import {Text} from '@/components/ui/text';
 import {VStack} from '@/components/ui/vstack';
 import {Block, InstalledApp} from '@/interfaces';
-import {EditIcon, EllipsisVertical, Trash} from 'lucide-react-native';
+import {Check, EditIcon, EllipsisVertical, Trash} from 'lucide-react-native';
+import {useTranslation} from 'react-i18next';
+import {TouchableOpacity} from 'react-native';
 
 interface BlockSavedProps {
   block: Block;
@@ -19,9 +23,48 @@ export const BlockSaved = ({
   onEdit,
   onDelete,
 }: BlockSavedProps) => {
+  const {t} = useTranslation('blocks');
+
+  const isActive = block.isActive;
+  const isAppsBlocked = block.blockApps;
+  const isNotificationsBlocked = block.blockNotifications;
+
   return (
-    <VStack className="p-4 bg-gray-600/40 rounded-2xl">
-      <Text>{block.name}</Text>
+    <VStack className="p-4 bg-gray-950 border border-custom-pink/50 rounded-2xl">
+      <Text className="font-bold text-xl mb-3">{block.name}</Text>
+      <HStack className="gap-4 mb-5">
+        <Badge
+          className="w-20 rounded-xl "
+          size="md"
+          variant="solid"
+          action={isActive ? 'success' : 'muted'}>
+          <BadgeIcon as={Check} className="mr-2" />
+          <BadgeText className={isActive ? `text-custom-green` : ''}>
+            {t(isActive ? 'active' : 'inactive')}
+          </BadgeText>
+        </Badge>
+        <Badge
+          className="w-20 rounded-xl"
+          size="md"
+          variant="solid"
+          action={isAppsBlocked ? 'success' : 'muted'}>
+          <BadgeIcon as={Check} className="mr-2" />
+          <BadgeText className={isAppsBlocked ? `text-custom-green` : ''}>
+            {t('apps')}
+          </BadgeText>
+        </Badge>
+        <Badge
+          className="min-w-20 rounded-xl"
+          size="md"
+          variant="solid"
+          action={isNotificationsBlocked ? 'success' : 'muted'}>
+          <BadgeIcon as={Check} className="mr-2" />
+          <BadgeText
+            className={isNotificationsBlocked ? `text-custom-green` : ''}>
+            {t('notifications')}
+          </BadgeText>
+        </Badge>
+      </HStack>
       <SelectedAppsInBlock
         packageNames={block.blockedApps}
         installedApps={allApps}
@@ -32,13 +75,18 @@ export const BlockSaved = ({
         offset={-23}
         trigger={({...triggerProps}) => {
           return (
-            <Button
-              size="xs"
-              variant="outline"
+            <TouchableOpacity
+              activeOpacity={0.95}
               {...triggerProps}
-              className="absolute top-2 right-2 border-0 rounded-full bg-red-100">
-              <ButtonIcon as={EllipsisVertical} size="sm" />
-            </Button>
+              style={{
+                position: 'absolute',
+                top: 8,
+                right: 8,
+              }}>
+              <Box className="p-2 border-0 rounded-full bg-custom-pink">
+                <EllipsisVertical size={15} />
+              </Box>
+            </TouchableOpacity>
           );
         }}>
         <MenuItem
@@ -46,16 +94,16 @@ export const BlockSaved = ({
           className="gap-3"
           textValue="Edit"
           onPress={onEdit}>
-          <EditIcon size={15} />
-          <MenuItemLabel size="sm">Edit</MenuItemLabel>
+          <EditIcon size={15} color="white" />
+          <MenuItemLabel size="sm">{t('edit')}</MenuItemLabel>
         </MenuItem>
         <MenuItem
           key="delete"
           className="gap-3"
           onPress={onDelete}
           textValue="Delete">
-          <Trash size={15} />
-          <MenuItemLabel size="sm">Delete</MenuItemLabel>
+          <Trash size={15} color="white" />
+          <MenuItemLabel size="sm">{t('delete')}</MenuItemLabel>
         </MenuItem>
       </Menu>
     </VStack>
